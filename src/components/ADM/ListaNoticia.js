27475bcfbae5075a677a907e86dedbbfb6d90ab3
Florencia norry <maflorencia.norry@gmail.com>
2021-05-23 18:50:42 -0300

    se termina la logica de los requerimientos solicitados

--- src/components/ADM/ListaNoticia.js
+++ src/components/ADM/ListaNoticia.js
@@ -1,38 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, InputGroup, FormControl } from "react-bootstrap";
 import ItemNoticia from "./ItemNoticia";
 
 const ListaNoticia = (props) => {
-  // const [buscar, setBuscar] = useState("");
   return (
     <Container>
       <h1 className="text-center my-4 fuente">Lista de Noticias</h1>
       <InputGroup className="my-3 w-25 fuente">
         <InputGroup.Prepend>
-          <InputGroup.Text          
-            // onChange={(e) => {
-            //   setBuscar(e.target.value);
-            // }}
-          >
-            Buscar
-          </InputGroup.Text>
+          <InputGroup.Text>Buscar</InputGroup.Text>
         </InputGroup.Prepend>
         <FormControl
           placeholder="Ingrese una categoria"
+          // onChange={handleBuscador}
         />
       </InputGroup>
       <section className="row">
-        {props.noticias
-          // .filter((nota) => {
-          //   if (buscar === "") {
-          //     return nota;
-          //   } else if (
-          //     nota.categoriaNoticia.toLowerCase().includes(buscar.toLowerCase())
-          //   ) {
-          //     return nota;
-          //   }
-          // })
-          .map((nota, indice) => (
+        {
+          props.noticias.map((nota, indice) => (
             <ItemNoticia
               key={indice}
               dato={nota}
