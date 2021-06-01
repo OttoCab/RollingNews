4121a67dae5e06c9674e473bc95099271d574659
Florencia norry <maflorencia.norry@gmail.com>
2021-06-01 17:31:40 -0300

    se sube completo sin login

--- src/components/ADM/Login.js
+++ src/components/ADM/Login.js
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 
 const Login = () => {
+
   return (
-    <Form className="w-50 ">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Usuario</Form.Label>
-        <Form.Control type="text" placeholder="Ingresar Usuario" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label=" Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <Container>
+      <Form className="w-50 ">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Usuario</Form.Label>
+          <Form.Control type="text" placeholder="Ingresar Usuario" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label=" Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
