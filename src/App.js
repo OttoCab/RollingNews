8462cee1068467a4ff6c15714165d141ed4652b3
Oscar Cabanillas <oskbanillas@gmail.com>
2021-06-01 20:08:41 -0300

    Se realizo un ABM para categorias

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //import NavegacionAdmin from "./components/ADM/NavegacionAdmin";
 import Footer from "./components/common/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ListaNoticia from "./components/ADM/ListaNoticia";
 import AgregarNoticia from "./components/ADM/AgregarNoticia";
 import AgregarCategoria from "./components/ADM/AgregarCategoria";
+import ListaNoticia from "./components/ADM/ListaNoticia";
 import ListaCategorias from "./components/ADM/ListaCategorias";
 import EditarNoticia from "./components/ADM/EditarNoticia";
+import EditarCategoria from "./components/ADM/EditarCategoria"
+import DetalleCategoria from "./components/ADM/DetalleCategoria";
 import { useState, useEffect } from "react";
 import React from "react";
 import Error404 from "./components/Error404";
-import DetalleCategoria from "./components/ADM/DetalleCategoria";
 import Inicio from "./components/Inicio.js";
 import Contacto from "./components/Contacto";
 import ADN from "./components/ADN";
 import Navegacion from "./components/common/Navegacion";
+
 
 function App() {
   const URL = process.env.REACT_APP_API_URL;
@@ -52,28 +54,27 @@ function App() {
           <Inicio noticias={noticias} consultarAPI={consultarAPI}></Inicio>
         </Route>
         <Route exact path="/Noticias">
-          <ListaNoticia
-            noticias={noticias}
-            consultarAPI={consultarAPI}
-          ></ListaNoticia>
+          <ListaNoticia noticias={noticias} consultarAPI={consultarAPI}>
+          </ListaNoticia>
+        </Route>
+        <Route exact path="/Categorias">
+          <ListaCategorias Categorias={Categorias} consultarAPI={consultarAPI}>          
+          </ListaCategorias>
         </Route>
         <Route exact path="/Noticia/nuevo">
-          <AgregarNoticia
-            consultarAPI={consultarAPI}
-            Categorias={Categorias}
-            noticias={noticias}
-          ></AgregarNoticia>
-        </Route>
-        <Route exact path="/Noticia/editar/:idNoticia">
-          <EditarNoticia consultarAPI={consultarAPI}></EditarNoticia>
+          <AgregarNoticia consultarAPI={consultarAPI} Categorias={Categorias} noticias={noticias}>
+          </AgregarNoticia>
         </Route>
         <Route exact path="/Categorias/nuevaCategoria">
           <AgregarCategoria consultarAPI={consultarAPI}></AgregarCategoria>
         </Route>
-        <Route exact path="/Categorias">
-          <ListaCategorias Categorias={Categorias}></ListaCategorias>
+        <Route exact path="/Noticia/editar/:idNews">
+          <EditarNoticia consultarAPI={consultarAPI}></EditarNoticia>
         </Route>
-        <Route exact path="/Categorias/detalleCategoria/:idCategoriaNoticia">
+        <Route exact path="/Categorias/editar/:idCategoria">
+          <EditarCategoria consultarAPI={consultarAPI}></EditarCategoria>
+        </Route>
+        <Route exact path="/Categorias/detalleCategoria/:idCategoria">
           <DetalleCategoria
             noticias={noticias}
             Categorias={Categorias}
