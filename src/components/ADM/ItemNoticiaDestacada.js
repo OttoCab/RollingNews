1608b616c45048e73a0f5d37185c644e7d10fcd8
Florencia norry <maflorencia.norry@gmail.com>
2021-06-10 20:33:27 -0300

    se sube con modificaciones

--- src/components/ADM/ItemNoticiaDestacada.js
+++ src/components/ADM/ItemNoticiaDestacada.js
@@ -4,18 +4,18 @@ import { Carousel } from "react-bootstrap";
 const ItemNoticiaDestacada = (props) => {
   return (
     <Carousel fade>
-      {props.noticiasDestacadas.map((detalleCat, indice) => (
+      {props.noticiasDestacadas.map((destacado, indice) => (
           <Carousel.Item className="size-adjust-car">
           <span className="position-absolute bg-warning mt-4 px-5 py-1 tag">
             Destacado
           </span>
           <img
             className="d-block w-100"
-            src={detalleCat.imagenNoticia}
+            src={destacado.imagenNoticia}
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>{detalleCat.tituloNoticia}</h3>
+            <h3>{destacado.tituloNoticia}</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
