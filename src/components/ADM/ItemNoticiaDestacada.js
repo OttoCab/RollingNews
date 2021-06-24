ce0ed985ac51c7e81844ef19794c8b84ddf004f4
emalderete <nicolas.alderete95@live.com>
2021-06-23 22:36:07 -0300

    Cambios en la página de inicio
    Se modifican las listas de categorías con nuevos estilos más amigables.

--- src/components/ADM/ItemNoticiaDestacada.js
+++ src/components/ADM/ItemNoticiaDestacada.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Carousel } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import "../assets/css/inicio.css";
 
 const ItemNoticiaDestacada = (props) => {
@@ -10,7 +10,7 @@ const ItemNoticiaDestacada = (props) => {
           <span className="bg-warning mt-4 px-5 py-1 tag">
             Destacado
           </span>
-          <img className="d-block w-100" src={destacado.imagenNoticia} alt="First slide"/>
+          <img className="d-block w-100 img-car" src={destacado.imagenNoticia} alt="First slide"/>
           <div className="overlay-car"></div>
           <Carousel.Caption>
             <h2>{destacado.tituloNoticia}</h2>
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,19 +1,12 @@
 import React from "react";
-import { Card, Button, Container, Carousel, CardDeck } from "react-bootstrap";
+import { Card, Button, Container, Carousel, ProgressBar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-// import DefectoImg from "./assets/img/defecto800x600.jpg";
-// import Defectowide from "./assets/img/defecto-wide800x394.jpg";
-// import DefectoMini from "./assets/img/defecto281x160.jpg";
 import covidimg from "./assets/img/covid-icon.svg";
 import adBanner1 from "./assets/img/adv-banner1.jpg";
 import adBanner2 from "./assets/img/adv-banner2.gif";
-// import adBanner3 from "./assets/img/adv-banner3.jpg";
 import adBanner4 from "./assets/img/adv-banner4.gif";
 import "./assets/css/inicio.css";
-// import error404 from "./Error404";
-// import { useParams } from "react-router-dom";
 import ItemNoticiaInicio from "./ADM/ItemNoticiaInicio";
-// import ItemNoticia from './ADM/ItemNoticia';
 import TiempoMoneda from "./ADM/TiempoMoneda";
 import ItemNoticiaDestacada from "../components/ADM/ItemNoticiaDestacada";
 
@@ -27,67 +20,7 @@ const Inicio = (props) => {
         <Container className="my-5 base-container">
         <h1 className="titles">Ultima hora...</h1>
           {/* <Carousel fade> */}
-                <ItemNoticiaDestacada
-                  noticiasDestacadas={props.noticiasDestacadas}
-                ></ItemNoticiaDestacada>
-
-          {/* {props.noticiasDestacadas.map((detalleCat, indice) => (
-                <ItemNoticiaDestacada
-                  key={indice}
-                  dato={detalleCat}
-                  Categorias={props.Categorias}
-                ></ItemNoticiaDestacada>
-              ))} */}
-
-          {/* <Carousel.Item className="size-adjust-car">
-              <span className="position-absolute bg-warning mt-4 px-5 py-1 tag">
-                Destacado
-              </span>
-              <img
-                className="d-block w-100"
-                src="https://img.lagaceta.com.ar/fotos/notas/2021/06/04/argentina-se-suma-produccion-sputnik-v-896799-231540.jpg"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item> */}
-          {/* <Carousel.Item className="size-adjust-car">
-              <span className="position-absolute bg-warning mt-4 px-5 py-1 tag">
-                Destacado
-              </span>
-              <img
-                className="d-block w-100"
-              
-                src="https://img.lagaceta.com.ar/fotos/notas/2021/05/30/avance-llegan-215-millones-vacunas-astrazeneca-pais-superara-175-millones-dosis-895957-191703.jpg"
-                alt="Second slide"
-              />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item className="size-adjust-car">
-              <span className="position-absolute bg-warning mt-4 px-5 py-1 tag">
-                Destacado
-              </span>
-              <img
-                className="d-block w-100"
-                src="https://img.lagaceta.com.ar/fotos/notas/2021/05/30/avance-llegan-215-millones-vacunas-astrazeneca-pais-superara-175-millones-dosis-895957-191703.jpg"
-                alt="Third slide"
-              />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item> */}
-          {/* </Carousel> */}
+                <ItemNoticiaDestacada noticiasDestacadas={props.noticiasDestacadas}></ItemNoticiaDestacada>
         </Container>
       </section>
       <Container className="my-5 base-container">
@@ -164,13 +97,15 @@ const Inicio = (props) => {
           </div>
         </section>
         <hr className="my-5" />
+        {/**************** Categorías de la página de inicio ********************/}
         <section className="row">
-          <div className="col-lg-9 col-sm-12 texts-small">
-            <Card className="shadow containerAnimation col-sm-12 my-3">
-              <h4 className="font-weight-bold mt-2">
-                <i className="titles">Actualidad</i>
+          <div className="col-lg-9 col-sm-12">
+              <h4 className="font-weight-bold mt-2 titles text-success">
+                Actualidad
               </h4>
-              <CardDeck className="container my-3 row">
+              <ProgressBar className="title-line" variant="success" now={10}/>
+            <div className="col-sm-12 my-3">
+              <div className="container my-3 row">
                 {props.noticias
                   .filter((cat) => {
                     if (cat.idCategoriaNoticia === "60b7d6acfeb96a24dce5c898") {
@@ -184,13 +119,15 @@ const Inicio = (props) => {
                       Categorias={props.Categorias}
                     ></ItemNoticiaInicio>
                   ))}
-              </CardDeck>
-            </Card>
-            <Card className="shadow my-3 containerAnimation">
-              <h4 className="font-weight-bold ml-2 mt-2">
-                <i className="titles">Salud</i>
+              </div>
+            </div>
+            <hr/>
+            <div className="my-3">
+              <h4 className="font-weight-bold mt-2 text-danger titles">
+                Salud
               </h4>
-              <CardDeck className="container my-3 row">
+              <ProgressBar className="title-line" variant="danger" now={10}/>
+              <div className="container my-3 row">
                 {props.noticias
                   .filter((cat) => {
                     if (cat.idCategoriaNoticia === "6") {
@@ -204,13 +141,15 @@ const Inicio = (props) => {
                       Categorias={props.Categorias}
                     ></ItemNoticiaInicio>
                   ))}
-              </CardDeck>
-            </Card>
-            <Card className="shadow containerAnimation">
-              <h4 className="font-weight-bold ml-2 mt-2">
-                <i className="titles">Economía</i>
+              </div>
+            </div>
+            <hr/>
+            <div>
+              <h4 className="font-weight-bold text-warning mt-2 titles">
+                Economía
               </h4>
-              <CardDeck className="container my-3 row">
+              <ProgressBar className="title-line" variant="warning" now={10}/>
+              <div className="container my-3 row">
                 {props.noticias
                   .filter((cat) => {
                     if (cat.idCategoriaNoticia === "5") {
@@ -224,18 +163,20 @@ const Inicio = (props) => {
                       Categorias={props.Categorias}
                     ></ItemNoticiaInicio>
                   ))}
-              </CardDeck>
-            </Card>
+              </div>
+            </div>
+            <hr/>
             <NavLink to="*">
               <div className="my-3">
                 <Card.Img src={adBanner2}></Card.Img>
               </div>
             </NavLink>
-            <Card className="shadow containerAnimation mt-3">
-              <h4 className="font-weight-bold ml-2 mt-2">
-                <i className="titles">Política</i>
+            <div className="mt-3">
+              <h4 className="font-weight-bold text-info mt-2 titles">
+                Política
               </h4>
-              <CardDeck className="container my-3 row">
+              <ProgressBar className="title-line" variant="info" now={10}/>
+              <div className="container my-3 row">
                 {props.noticias
                   .filter((cat) => {
                     if (cat.idCategoriaNoticia === "7") {
@@ -249,13 +190,15 @@ const Inicio = (props) => {
                       Categorias={props.Categorias}
                     ></ItemNoticiaInicio>
                   ))}
-              </CardDeck>
-            </Card>
-            <Card className="shadow containerAnimation mt-3">
-              <h4 className="font-weight-bold ml-2 mt-2">
-                <i className="titles">Deportes</i>
+              </div>
+            </div>
+            <hr/>
+            <div className="mt-3">
+              <h4 className="font-weight-bold text-primary mt-2 titles">
+                Deportes
               </h4>
-              <CardDeck className="container my-3 row">
+              <ProgressBar className="title-line" variant="primary" now={10}/>
+              <div className="container my-3 row">
                 {props.noticias
                   .filter((cat) => {
                     if (cat.idCategoriaNoticia === "60b58135d63ea4368c0cf559") {
@@ -269,8 +212,8 @@ const Inicio = (props) => {
                       Categorias={props.Categorias}
                     ></ItemNoticiaInicio>
                   ))}
-              </CardDeck>
-            </Card>
+              </div>
+            </div>
             <NavLink to="/Categorias" className="text-decoration-none">
               <Button className="mt-4 btn-grad" variant="info" block>
                 <h5>Ver todas las categorías</h5>
